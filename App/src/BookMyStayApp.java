16efe90b24cd00c79646976538fc1480015dbd42
Inbasekaran12 <in5904@srmist.edu.in>
2026-03-16 00:45:57 +0530

    feat: add application entry point with welcome message

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,22 @@
+/**
+ * MAIN CLASS BookMyStayApp
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * Establishes the application starting point
+ * and prints a welcome message.
+ *
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    /**
+     * Application entry point.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
